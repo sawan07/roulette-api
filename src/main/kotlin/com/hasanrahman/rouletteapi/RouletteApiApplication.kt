99d50f35cfdf80a8f33d0e967fea780e9de83a5d
Hasan Rahman <hasanrahman@lacunaclientscope.com>
2018-11-05 21:19:16 +0000

    Initial commit

---
+++ src/main/kotlin/com/hasanrahman/rouletteapi/RouletteApiApplication.kt
@@ -0,0 +1,11 @@
+package com.hasanrahman.rouletteapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RouletteApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RouletteApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/hasanrahman/rouletteapi/RouletteApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.hasanrahman.rouletteapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RouletteApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
