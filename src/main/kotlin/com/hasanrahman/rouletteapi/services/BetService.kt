8d0eb836c9c87119638d237714240d469cbdcc8a
Hasan Rahman <hasanrahman@lacunaclientscope.com>
2018-11-06 20:54:19 +0000

    necessary comments added

--- src/main/kotlin/com/hasanrahman/rouletteapi/services/BetService.kt
+++ src/main/kotlin/com/hasanrahman/rouletteapi/services/BetService.kt
@@ -23,6 +23,12 @@ class BetService {
 		return getVersion(httpSession)
 	}
 	
+	/**
+	 * Getting the current version of roulette
+	 *  @param httpSession: HttpSession to save the version value
+	 *  @return saved version value
+	 *  @throws VersionNotFound exception
+	 */
 	fun getVersion(httpSession: HttpSession): String =
 		if (httpSession.getAttribute(Const.VERSION) != null)
 			httpSession.getAttribute(Const.VERSION).toString()
@@ -30,6 +36,15 @@ class BetService {
 			"version not set"
 		)
 	
+	/**
+	 * Running the roulette randomizer for outside Bets and returning the winning value
+	 * @param amount Double: bet amount
+	 * @param outsideBets OutsideBets enum : choice of outside Bet
+	 * @param httpSession: HttpSession to save the version value
+	 * @return winning amount as Double
+	 * All outside bets has a very well defined sets of slot. The random number is checked
+	 * inside the list based on the outside bet type and thus the winning is calculated.
+	 */
 	fun outsideBets(amount: Double, outsideBets: OutsideBets, httpSession: HttpSession): Double {
 		val number = randomize(getVersion(httpSession)).toInt()
 		logger.info("rolled number is $number")
@@ -133,14 +148,37 @@ class BetService {
 		}
 	}
 	
+	/**
+	 * Randimizer mechanism
+	 * @param version String: current roulette version
+	 * @return randomized number in string format
+	 */
 	private fun randomize(version: String): String {
+		//EU version roulette slots range from 0 to 36. So, a basic number randomizer is used.
 		return if (version == Versions.EU_VERSION.toString())
 			Random().nextInt(37).toString()
+		
+		/*
+		US version roulette has an additional 00 slot. That's why a custom numbers list of string is used
+		to get the random slot. US version has total 38 slots. Due to the US version the randomizer is returning
+		a string value instead of Int. If 00 is converted to Int, it will become Int(0) instead of Int(00).
+		*/
 		else {
 			numbers[Random().nextInt(38)]
 		}
 	}
 	
+	/**
+	 * Running the roulette randomizer for inside Bets and returning the winning value
+	 * @param amount Double: bet amount
+	 * @param insideBets InsideBets enum : choice of inside Bet
+	 * @param httpSession: HttpSession to save the version value
+	 * @return winning amount as Double
+	 * Inside bets do not have a well defined sets of slot. The slots depend on the user
+	 * input. The user/Front-end needs to give the set of slots based on the type of inside
+	 * bet. The randmizer generates a random number and that is checked with the user inserted
+	 * array of slots. Based on the result of that, the winning is calculated.
+	 */
 	fun insideBets(amount: Double, insideBets: InsideBets, numbers: Array<String>, httpSession: HttpSession): Double {
 		val version = getVersion(httpSession)
 		val number = randomize(version)
@@ -153,6 +191,7 @@ class BetService {
 				InsideBets.SQUARE -> amount * 9
 				InsideBets.DOUBLE_STREET -> amount * 6
 				InsideBets.TRIO -> amount * 12
+				//US and EU version returns different payout for BASKET as US version has an extra 00
 				InsideBets.BASKET -> if (version == Versions.EU_VERSION.toString()) amount * 9 else amount * 7
 			}
 		} else 0.00
